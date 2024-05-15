88d874ca9a7c7de72c56ed3550674db1f534e999
DaniilZinoviev05 <danielzinoviev@yandex.ru>
2024-05-16 02:15:53 +0300

    modified console.log

--- main.js
+++ main.js
@@ -136,6 +136,7 @@ class HousingData {
         for (let i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
+        console.log(color)
         return color;
     }
 }
